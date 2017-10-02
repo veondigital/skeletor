b5fd0ce8ceb4b4c6695eda18caf420ea9dc8a090
Manuel.Rubio <manuel.rubio@veon.com>
2017-10-02 12:38:11 +0200

    bugfix affecting to the dependencies, release and testing

--- test/veon_app_tests.erl
+++ test/veon_app_tests.erl
@@ -5,6 +5,6 @@
 
 start_stop_test_() ->
     {timeout, 10000, fun() ->
-        {ok, [veon]} = application:ensure_all_started(veon),
+        {ok, _} = application:ensure_all_started(veon),
         ok = application:stop(veon)
     end}.
